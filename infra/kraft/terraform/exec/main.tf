675d74630154609dc6a389a42e13935f9be11ed2
alex carvalho <alex.carvaho.data@gmail.com>
2023-11-11 20:19:32 +0100

    feat: add terraform exec - create network and image

---
+++ infra/kraft/terraform/exec/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+#####################################################
+# network
+#####################################################
+resource "docker_network" "kafka_network" {
+  name = "kafka-network"
+}
+
+#####################################################
+# image
+#####################################################
+resource "docker_image" "kafka_node" {
+  name         = "oak/kafka-node:latest"
+  keep_locally = true
+}
