3867480e0656ea6b9317887ac0d3ee547ff350a7
alex carvalho <alex.carvalho.data@gmail.com>
2023-11-18 20:34:28 +0100

    feat: add controller to kafka build

--- infra/kraft/terraform/build/images.tf
+++ infra/kraft/terraform/build/images.tf
@@ -1,11 +1,25 @@
 resource "docker_image" "kafka_node" {
-  name       = "kafka-node"
+  name         = "oak/kafka-node"
+  force_remove = true
   build {
     context = "../../docker/kafka-node/"
     build_arg = {
       SCALA_VERSION : "2.13"
       KAFKA_VERSION : "3.6.0"
     }
-    tag  = ["oak/kafka-node:latest", "oak/kafka-node:3.6.0-1.0.0"]
+    # tag = ["oak/kafka-node:3.6.0-0.2.0"]
+  }
+}
+
+resource "docker_image" "kafka_controller" {
+  name         = "oak/kafka-controller"
+  depends_on   = [docker_image.kafka_node]
+  force_remove = true
+  build {
+    context = "../../docker/kafka-controller/"
+    build_arg = {
+      SCALA_VERSION : "2.13"
+      KAFKA_VERSION : "3.6.0"
+    }
   }
 }
