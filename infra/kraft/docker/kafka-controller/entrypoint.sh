44fead38ff02366f1cf7874a8eb00cebd51319ad
alex carvalho <alex.carvalho.data@gmail.com>
2023-11-18 20:27:11 +0100

    refactor: complete entrypoint.sh for kafka-controller image

--- infra/kraft/docker/kafka-controller/entrypoint.sh
+++ infra/kraft/docker/kafka-controller/entrypoint.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
-echo "kafka-controller entry point script"
-
-
-############################# Server Basics #############################
-
-# The role of this server. Setting this puts us in KRaft mode
-# process.roles=controller
-
-# The node id associated with this instance's roles
-# node.id=1
-
-# The connect string for the controller quorum
-# controller.quorum.voters=1@localhost:9093
-
-# log.dirs=/tmp/kraft-controller-logs
-# log.dirs=/tmp/kraft-broker-logs
-
-# shell test command
-# ./entrypoint.sh --cluster-id 123 --process-roles controller --node-id 11 --controller-quorum-voters 11@localhost:9093
-
 
 print_parsed_args () {
     echo ""
     echo "cluster_id: $cluster_id"
-    echo "process_roles: $process_roles"
     echo "node_id: $node_id"
     echo "controller_quorum_voters: $controller_quorum_voters"
     echo ""
 }
+
 
 print_help () {
     echo ""
@@ -36,14 +16,23 @@ print_help () {
     echo ""
     echo "Options:"
     echo " -c, --cluster-id                 kafka cluster uuid"
+    echo "                                  example:"
+    echo "                                    --cluster-id OO7uy3mpQvieepZMySnprQ"
     echo " -n, --node-id                    controller node id"
+    echo "                                  example:"
+    echo "                                    --node-id 11"
     echo " -q, --controller-quorum-voters   comma separated list of quorum voters."
     echo "                                  pattern:"
-    echo "                                   [node id]@[host]:[port]"
+    echo "                                    single node:"
+    echo "                                      [node id]@[host]:[port]"
+    echo "                                    multiple nodes:"
+    echo "                                      [node id]@[host]:[port],[node id]@[host]:[port]"
+    echo "                                  example:"
+    echo "                                    --controller-quorum-voters 11@localhost:9093,12@controller12:9093"
     echo " -h, --help                       display this help"
     echo ""
     echo "Example:"
-    echo " :/# ./entrypoint.sh --cluster-id 123 --node-id 11 --controller-quorum-voters 11@localhost:9093,12@controller12:9093,13@controller13:9093"
+    echo " ./entrypoint.sh --cluster-id OO7uy3mpQvieepZMySnprQ --node-id 11 --controller-quorum-voters 11@localhost:9093,12@controller12:9093,13@controller13:9093"
 
     exit 0
 }
@@ -66,5 +55,46 @@ parse_args()
     done
 }
 
+
+generate_controller_properties()
+{
+    properties_file_name=controller_${node_id}.properties
+
+    sed \
+      -e s/node.id=1/node.id=$node_id/ \
+      -e s/controller.quorum.voters=1@localhost:9093/controller.quorum.voters=$controller_quorum_voters/ \
+      ${KAFKA_HOME}/config/kraft/controller.properties > ${KAFKA_HOME}/config/kraft/$properties_file_name
+}
+
+
+format_controller_log_dir()
+{
+    echo "formatting controller log dir"
+
+    kafka-storage.sh format -t $cluster_id -c $KAFKA_HOME/config/kraft/$properties_file_name
+}
+
+
+start_kafka_controller()
+{
+    echo "starting kafka controler $node_id"
+
+    kafka-server-start.sh ${KAFKA_HOME}/config/kraft/$properties_file_name
+}
+
+print_header()
+{
+    echo ""
+    echo "kafka-controller entry point script"
+}
+
+print_header
+
 parse_args $@
 print_parsed_args
+
+generate_controller_properties
+
+format_controller_log_dir
+
+start_kafka_controller
